1326a500071a42c4f2a6bd560efa91bd44fd368d
filippaios <64655577+filippaios@users.noreply.github.com>
2022-04-11 13:55:47 +0300

    Mexri 10.1

--- src/mainpackage/CreateUsers.java
+++ src/mainpackage/CreateUsers.java
@@ -3,12 +3,22 @@ package mainpackage;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.*;
 
 public class CreateUsers {
 
 	public static void main(String[] args) {
+		
+		 List<Users> userList = new ArrayList<Users>();		
+		 List<Students> studentList = new ArrayList<Students>();
+		 List<Secretaries> secretariesList = new ArrayList<Secretaries>();
+		 List<Professors> professorList = new ArrayList<Professors>();
+		 List<Grades> gradeList = new ArrayList<Grades>();
+		 List<Courses> coursesList = new ArrayList<Courses>(); 
+		 
 
 		// USER
 		Scanner input = new Scanner(System.in);
@@ -47,6 +57,7 @@ public class CreateUsers {
 			}
 
 			Users xrhsths = new Users(name, surname, username, department);
+			userList.add(xrhsths);
 			System.out.println("Ο Χρήστης είναι ο " + xrhsths.getName() + "" + xrhsths.getSurname() + "Και ειναι στο"
 					+ xrhsths.getDepartment() + "τμήμα");
 
@@ -101,6 +112,7 @@ public class CreateUsers {
 			Students foithths = new Students(names_student, surname_student, username_student, department_student,
 					Integer.parseInt(registrationNumber));
 			foithths.Show_User();
+			studentList.add(foithths);
 
 		} catch (Exception e) {
 			System.out.println("General Exception occurred" + e);
@@ -108,26 +120,63 @@ public class CreateUsers {
 			System.out.println("H Εγγραφή του Φοιτητή τελείωσε!");
 		}
 
-		//PROFESSOR
-		Professors kathigitis = new Professors ("Γιώργος", "Φιλιππαίος", "filippaios", "Πληροφορικής", "java");
-		//Secreteries.
-		Secretaries grammateia = new Secretaries ("Κική", "Βουγιουκλάκη", "kiki", "Πληροφορικής");
+		// PROFESSOR
+		Professors kathigitis = new Professors("Γιώργος", "Φιλιππαίος", "filippaios", "Πληροφορικής", "java");
+		professorList.add(kathigitis);
 		
-		//9.1,   9.2
-		try{
-		    FileInputStream fstream = new FileInputStream("/JavaBasics/src/mainpackage/Student.txt");
-		    DataInputStream in = new DataInputStream(fstream);
-		    BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		    String strLine;
-		    while ((strLine = br.readLine()) != null)   {
-		       String[] tokens = strLine.split(" ");
-		       int number = Integer.parseInt(tokens[4]);
-		       Students record = new Students(tokens[0],tokens[1],tokens[2],tokens[3],number);
-		    }
-		    in.close();
-		    } catch (Exception e){
-		      System.err.println("Kάτι πήγε Λάθος: " + e.getMessage());
-		    }
+		// Secreteries.
+		Secretaries grammateia = new Secretaries("Κική", "Βουγιουκλάκη", "kiki", "Πληροφορικής");
+		secretariesList.add(grammateia);
+		
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		// 9.1, 9.2
+		try {
+			FileInputStream fstream = new FileInputStream("/src/mainpackage/Student.txt");
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				String[] tokens = strLine.split("/");
+				int number = Integer.parseInt(tokens[4]);
+
+				if ((tokens[0].matches("^[α-ωΑ-Ωa-zA-Z]*$"))) {
+				} else {
+					throw new Exception("το όνομα δεν είναι σωστό");
+				}
+				if ((tokens[1].matches("^[α-ωΑ-Ωa-zA-Z]*$"))) {
+				} else {
+					throw new Exception("το επίθετο δεν είναι σωστό");
+				}
+				if ((tokens[2].matches("^[α-ωΑ-Ωa-zA-Z]*$"))) {
+				} else {
+					throw new Exception("το username δεν είναι σωστό");
+				}
+				if ((tokens[3].matches("^[α-ωΑ-Ωa-zA-Z]*$"))) {
+				} else {
+					throw new Exception("το Τμήμα δεν είναι σωστό");
+				}
+				if (tokens[4].matches("^[0-9]*$")) {
+
+				} else {
+					throw new Exception("Ο Αριθμός Μητρώου δεν είναι σωστός");
+				}			
+				
+
+				Students record = new Students(tokens[0], tokens[1], tokens[2], tokens[3], number);
+				System.out.println("H Εγγραφή του Φοιτητή τελείωσε!" + record.getRegistrationNumber());
+			}
+			in.close();
+		} catch (Exception e) {
+			System.err.println("Kάτι πήγε Λάθος: " + e.getMessage());
+		}
+		
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//10.0
+		
+		grammateia.Dimiourgia_kathigith(professorList);
+		
+		
+		
 
 	}
 
--- src/mainpackage/Secretaries.java
+++ src/mainpackage/Secretaries.java
@@ -1,6 +1,12 @@
 package mainpackage;
 
-  class Secretaries extends Users {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Secretaries extends Users {
+	  
+	  Scanner input = new Scanner(System.in);
 
 	public Secretaries(String name,String surname,String username,String department) {
 		super(name,surname,username,department);
@@ -10,5 +16,111 @@ package mainpackage;
 	public void Show_Grammateia() {
 		System.out.println("Εδώ ειναι λειτουργίες της Γραμματείας  " );
 	}
+	
+	public void Dimiourgia_kathigith( List<Professors> lista_kathigitwn) {
+		try {
+		System.out.println("Δώστε το όνομα του Καθηγητή: ");
+		String name_kathigiti = input.nextLine();
+		if (name_kathigiti.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
 
+		} else {
+			throw new Exception("το όνομα δεν είναι σωστό");
+		}
+
+		System.out.println("Δώστε το επίθετο του Καθηγητή ");
+		String surname_kathigiti = input.nextLine();
+		if (surname_kathigiti.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
+
+		} else {
+			throw new Exception("το επίθετο δεν είναι σωστό");
+		}
+
+		System.out.println("Δώστε το username του Καθηγητή: ");
+		String username_kathigiti = input.nextLine();
+		if (username_kathigiti.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
+
+		} else {
+			throw new Exception("το username δεν είναι σωστό");
+		}
+
+		System.out.println("Δώστε το τμήma που διδάσκει: ");
+		String department_kathigiti = input.nextLine();
+		if (department_kathigiti.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
+		} else {
+			throw new Exception("το τμήμα  δεν είναι σωστό");
+		}		
+		System.out.println("Δώστε το μάθημα που διδάσκει: ");
+		String mathima_kathigiti = input.nextLine();
+		if (department_kathigiti.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
+		} else {
+			throw new Exception("το μάθημα  δεν είναι σωστό");
+		}	
+		Professors kathigiths = new Professors(name_kathigiti, surname_kathigiti, username_kathigiti, department_kathigiti,department_kathigiti);
+		lista_kathigitwn.add(kathigiths);		
+		
+
+	} catch (Exception e) {
+		System.out.println("General Exception occurred" + e);
+	} 
+		
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void Dimiourgia_foithth( List<Students> lista_foithtwn) {
+		try {
+
+			System.out.println("Δώστε το όνομα του Φοιτητή: ");
+			String names_student = input.nextLine();
+			if (names_student.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
+
+			} else {
+				throw new Exception("το όνομα δεν είναι σωστό");
+			}
+
+			System.out.println("Δώστε το επίθετο του Φοιτητή ");
+			String surname_student = input.nextLine();
+			if (surname_student.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
+
+			} else {
+				throw new Exception("το επίθετο δεν είναι σωστό");
+			}
+
+			System.out.println("Δώστε το username του Φοιτητή: ");
+			String username_student = input.nextLine();
+			if (username_student.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
+
+			} else {
+				throw new Exception("το username δεν είναι σωστό");
+			}
+
+			System.out.println("Δώστε το τμήma που φοιτεί: ");
+			String department_student = input.nextLine();
+			if (department_student.matches("^[α-ωΑ-Ωa-zA-Z]*$")) {
+
+			} else {
+				throw new Exception("το τμήμα  δεν είναι σωστό");
+			}
+			System.out.println("Δώστε το registration number: ");
+			String registrationNumber = input.nextLine();
+			if (registrationNumber.matches("^[0-9]*$")) {
+
+			} else {
+				throw new Exception("Ο Αριθμός Μητρώου δεν είναι σωστός");
+			}
+
+			Students foithths = new Students(names_student, surname_student, username_student, department_student,
+					Integer.parseInt(registrationNumber));			
+			lista_foithtwn.add(foithths);
+
+		} catch (Exception e) {
+			System.out.println("General Exception occurred" + e);
+		} finally {
+			System.out.println("H Εγγραφή του Φοιτητή τελείωσε!");
+		}
+	}
+
+	
+
+	
 }
